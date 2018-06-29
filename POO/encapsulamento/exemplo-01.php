88980a3ae0bb1c7c35cfaff660053d37a9e17966
Vinicius Rodrigues <vinicrodsou@gmail.com>
2018-06-29 10:54:56 -0300

    Interfaces e classes abstratas

---
+++ POO/encapsulamento/exemplo-01.php
@@ -0,0 +1,19 @@
+<?php
+
+class Pessoa {
+
+	public $nome     = "Rasmus Lerdorf";
+	protected $idade = 48;
+	private $senha   = "123456";
+
+	
+	public function verDados() {
+		echo $this->nome."<br>";
+		echo $this->idade."<br>";
+		echo $this->senha."<br>";
+	}
+}
+
+$pessoa = new Pessoa();
+$pessoa->verDados();
+?>
